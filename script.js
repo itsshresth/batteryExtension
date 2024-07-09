353033ef9c43fbecfd04387e0786ea6078fa57eb
Shresth Gupta <132778393+itsshresth@users.noreply.github.com>
2024-07-09 06:00:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-/*=============== BATTERY ===============*/
 initBattery()
 
 function initBattery(){
@@ -9,29 +7,28 @@ function initBattery(){
     
     navigator.getBattery().then((batt) =>{
         updateBattery = () =>{
-            /* 1. We update the number level of the battery */
+          
             let level = Math.floor(batt.level * 100)
             batteryPercentage.innerHTML = level+ '%'
 
-            /* 2. We update the background level of the battery */
+           
             batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`
 
-            /* 3. We validate full battery, low battery and if it is charging or not */
-            if(level == 100){ /* We validate if the battery is full */
+          
+            if(level == 100){
                 batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`
-                batteryLiquid.style.height = '103%' /* To hide the ellipse */
+                batteryLiquid.style.height = '103%' 
             }
-            else if(level <= 20 &! batt.charging){ /* We validate if the battery is low */
+            else if(level <= 20 &! batt.charging){
                 batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`
             }
-            else if(batt.charging){ /* We validate if the battery is charging */
+            else if(batt.charging){ 
                 batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`
             }
-            else{ /* If it's not loading, don't show anything. */
+            else{
                 batteryStatus.innerHTML = ''
             }
             
-            /* 4. We change the colors of the battery and remove the other colors */
             if(level <=20){
                 batteryLiquid.classList.add('gradient-color-red')
                 batteryLiquid.classList.remove('gradient-color-orange','gradient-color-yellow','gradient-color-green')
@@ -51,7 +48,7 @@ function initBattery(){
         }
         updateBattery()
 
-        /* 5. Battery status events */
+       
         batt.addEventListener('chargingchange', () => {updateBattery()})
         batt.addEventListener('levelchange', () => {updateBattery()})
     })
